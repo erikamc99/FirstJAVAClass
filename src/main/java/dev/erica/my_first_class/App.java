f607d029797065f6d4cfca62afd09ac992af2fe6
Erica <you@example.com>
2025-01-29 16:49:33 +0100

    create branch dev

---
+++ src/main/java/dev/erica/my_first_class/App.java
@@ -0,0 +1,17 @@
+package dev.erica.my_first_class;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
